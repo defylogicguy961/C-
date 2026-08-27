2f782a6da4398accdf47fdb8275ae8787c4d52f1
Defylogicguy <ngtuanminh961@gmail.com>
2026-08-27 19:57:07 +0700

    update idk.cpp

---
+++ idk.cpp
@@ -0,0 +1,54 @@
+/*************************
+  Author: Defy logic guy
+  19:56:50 - 27/08/2026
+*************************/
+#include <bits/stdc++.h>
+using namespace std;
+ostream &operator<<(ostream &os, const string &s)
+{
+    os.write(s.data(), s.size());
+    return os;
+}
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+template <typename T>
+auto operator<<(ostream &os, const T &c) -> typename enable_if<!is_same<T, string>::value, decltype(c.begin(), c.end(), os)>::type
+{
+    for (auto it = c.begin(); it != c.end(); it++)
+        os << (it == c.begin() ? "{" : ", ") << *it;
+    return os << "}";
+}
+#define dbg(x) cerr << #x << " = " << (x) << '\n';
+#define int long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define maxall(x) *max_element(all(x))
+#define minall(x) *min_element(all(x))
+#define heap priority_queue
+#define pb emplace_back
+#define MOD 1000000007
+#define NAME "idk"
+
+void solve()
+{
+}
+
+signed main()
+{
+    if (fopen(NAME ".INP", "r"))
+    {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt = 1;
+    // cin >> tt;
+
+    while (tt--)
+        solve();
+
+    return 0;
+}
